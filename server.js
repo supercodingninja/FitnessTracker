c0fd6c7bcea3e13e2f20a1f927d24ca0578a9c6b
Frederick Thomas <supercodingninja@outlook.com>
2021-04-06 20:27:31 -0700

    SERIOUSLY?!  https://gist.github.com/leommoore/7524073

---
+++ server.js
@@ -0,0 +1,31 @@
+'use strict';
+
+// This is where you need to remember that weird name for a middleware request, "Morgan."  See Ref. https://www.npmjs.com/package/morgan This is the HTTP request logger middleware for node.js).
+const logger = require('morgan');
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const PORT = process.env.PORT || 3000;
+
+
+// See Ref. https://gist.github.com/leommoore/7524073 //
+app.use(logger('dev'));
+app.use(require('./routes/htmlRoutes.js'));
+app.use(require('./routes/apiRoutes.js'));
+app.use(express.static('./public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+mongoose.connect('mongodb://localhost/workout', {
+
+    useNewUrlParser: true,
+
+});
+
+
+app.listen(PORT, () => {
+  
+    console.log(`App running on port ${PORT}!`);
+
+});
