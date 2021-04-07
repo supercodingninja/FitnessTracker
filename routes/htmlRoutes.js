917dbf50e424740a087873fbd6c1d78039523cc0
Frederick Thomas <supercodingninja@outlook.com>
2021-04-06 20:01:54 -0700

    Completed the path to the document.  I think that I did everything.  I'm going to check all data.

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,23 @@
+'use strict';
+
+// Think!  What have we not done...Set up the path to the document! //
+const path = require('path');
+const router = require('express').Router();
+
+
+// This should render all stats to the document. //
+router.get('./stats'), (res) => {
+
+    res.sendFile(path.join(__dirname, '../public/stats.html'));
+
+};
+
+
+// I need to get the exercises displayed. //
+router.get('./exercise'), (res) => {
+
+    res.sendFile(path.join(__dirname, '../public/exercise.html'));
+
+};
+
+module.exports = router;
